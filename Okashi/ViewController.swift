4c1ab5253875639454384d7501cc31d6f94b0fcb
y-nakane <y-nakane@sakura.ad.jp>
2018-11-21 18:30:52 +0900

    取得したJSONレスポンスデータを記憶する構造体を宣言

--- Okashi/ViewController.swift
+++ Okashi/ViewController.swift
@@ -21,5 +21,50 @@ class ViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    // 検索ボタンをクリック（タップ）時
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // キーボードを閉じる
+        view.endEditing(true)
+        // サーチバーにテキストが入っているときにsearchWordに格納
+        if let searchWord = searchBar.text {
+            // デバッグエリアに出力
+            print(searchWord)
+            // 入力されていたら、お菓子を検索
+            searchOkashi(keyword: searchWord)
+        }
+    }
+    
+    // JSONのItem内のデータ構造
+    struct ItemJson: Codable {
+        // お菓子の名称
+        let name: String?
+        // メーカー
+        let maker: String?
+        // 掲載URL
+        let url: URL?
+        // 画像URL
+        let image: URL?
+    }
+    
+    // JSONのデータ構造
+    struct ResultJson: Codable {
+        // 複数要素
+        let item:[ItemJson]?
+    }
+    
+    // searchOkashiメソッド
+    // 第一引数：keyword 検索したいワード
+    func searchOkashi(keyword: String) {
+        // お菓子の検索キーワードをURLエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        // リクエストURLの組み立て
+        guard let req_url = URL(string: "http://www.sysbird.jp/webapi/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        // 出力
+        print(req_url)
+    }
 }
-
